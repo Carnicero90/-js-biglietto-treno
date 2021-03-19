fba311203176868a2e4cf8255e4160630eb7de0a
Carnicero90 <filippomontani82@gmail.com>
2021-03-19 17:13:06 +0100

    microfattorizzazione di script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 var coeffKm = 0.21; // prezzo al chilometro
-var senior = 65;
-var minor = 18;
-var coeffSenior = 0.6; // sconto over 65
-var coeffMinor = 0.8; //sconto under 18
+var senior = 65; // età minima per accedere a sconti senior
+var junior = 18; // età massima per accedere a sconti junior
+var scontoSenior = 0.4; // sconto senior
+var scontoMinor = 0.2; //sconto junior
 
+// Validazione dati utente
 alert("Grazie per aver scelto Trenord:\ninserire la propria età e i chilometri totali da percorrere (in formato numerico) per calcolare il prezzo del biglietto")
 var etaUtente = parseInt(prompt('Quanti anni hai?'));
 var kmPercorsi = parseInt(prompt('Quanti chilometri devi percorrere?'));
@@ -12,31 +13,29 @@ if (isNaN(etaUtente) || isNaN(kmPercorsi)) {
     alert('inseriti dati non validi, riprovare')
     location.reload();
 }
-//Calcolo prezzo base
+
+// Calcolo sconto
+var fattorePrezzo = 1;
+if (etaUtente < junior) {
+    fattorePrezzo -= scontoMinor;
+} else if (etaUtente > senior) {
+    fattorePrezzo -= scontoSenior;
+}
 var prezzo = kmPercorsi * coeffKm;
+
 document.getElementById('prezzo-base').innerHTML = prezzo.toLocaleString("it-IT", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
 });
-var scontoApplicato = "0%";
 
+prezzo *= fattorePrezzo;
+scontoApplicato = ((1 - fattorePrezzo) * 100).toFixed(0) + "%";
 
-// Calcolo sconti in base a età
-if (etaUtente < minor) {
-    prezzo = prezzo * coeffMinor;
-    scontoApplicato = "20%";
-} else if (etaUtente > senior) {
-    prezzo = prezzo * coeffSenior;
-    scontoApplicato = "40%";
-}
-
-// Arrotondo a 2 cifre decimali
 var prezzoFinale = prezzo.toLocaleString("it-IT", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
 });
 
-var messaggio = `spenderai ${prezzoFinale} euro`;
 document.getElementById('km').innerHTML = kmPercorsi.toLocaleString("it-IT");
 document.getElementById('sconto').innerHTML = scontoApplicato;
 document.getElementById('totale').innerHTML = prezzoFinale;
