c99e5bb9583de2b1ec91c12e8d0889fbfef0c9c1
Carnicero90 <filippomontani82@gmail.com>
2021-03-19 14:08:50 +0100

    aggiunta sorta di validazione

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var coeffKm = 0.21; // prezzo al chilometro
 var coeffOver65 = 0.6; // sconto over 65
 var coeffUnder18 = 0.8; //sconto under 18
-
-var etaUtente = parseInt(prompt("Quanti anni hai?"));
-var kmPercorsi = parseInt(prompt("Quanti chilometri devi percorrere?"));
-
+var etaUtente = parseInt(prompt('Quanti anni hai?'));
+var kmPercorsi = parseInt(prompt('Quanti chilometri devi percorrere?'));
+var messaggio;
 //Calcolo prezzo base
 var prezzo = kmPercorsi * coeffKm;
+
 
 // Calcolo sconti in base a età
 if (etaUtente < 18) {
@@ -19,5 +19,15 @@ if (etaUtente > 65) {
 // Arrotondo a 2 cifre decimali
 var prezzoFinale = prezzo.toFixed(2);
 
+
+messaggio = `spenderai ${prezzoFinale} euro`;
+
+if (isNaN(etaUtente) || isNaN(kmPercorsi)) {
+    // messaggio = "inseriti valori non validi, riprovare";
+    alert('inseriti dati non validi, riprovare')
+    location.reload();
+}
+// potrei farlo anche direttamente su prezzo/prezzoFinale
+
 // Alert utente spesa totale
-alert(`spenderai ${prezzoFinale} euro`);
+document.getElementById('spesa').innerHTML = messaggio;
