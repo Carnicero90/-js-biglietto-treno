fb24b8637a4d71dc338ada0b6a6c76decd09cee0
Carnicero90 <filippomontani82@gmail.com>
2021-03-19 13:53:38 +0100

    aggiunti commenti e cambiato nome alcune variabili in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-var coeffKm = 0.21;
-var coeffVeci = 0.6;
-var coeffGiovani = 0.8;
+var coeffKm = 0.21; // prezzo al chilometro
+var coeffOver65 = 0.6; // sconto over 65
+var coeffUnder18 = 0.8; //sconto under 18
 
-var etaUtente = parseInt(prompt("età"));
-var kmPercorsi = parseInt(prompt("quanti km"));
+var etaUtente = parseInt(prompt("Quanti anni hai?"));
+var kmPercorsi = parseInt(prompt("Quanti chilometri devi percorrere?"));
 
+//Calcolo prezzo base
 var prezzo = kmPercorsi * coeffKm;
 
+// Calcolo sconti in base a età
 if (etaUtente < 18) {
-    prezzo = prezzo * coeffGiovani;
+    prezzo = prezzo * coeffUnder18;
 }
 
 if (etaUtente > 65) {
-    prezzo = prezzo * coeffVeci;
+    prezzo = prezzo * coeffOver65;
 }
-
+// Arrotondo a 2 cifre decimali
 var prezzoFinale = prezzo.toFixed(2);
+
+// Alert utente spesa totale
 alert(`spenderai ${prezzoFinale} euro`);
