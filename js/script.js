cb1d63ec6d4d2c99183cea0458945c7dfb4d090b
Carnicero90 <filippomontani82@gmail.com>
2021-03-20 07:17:54 +0100

    semplificato un minimo la verifica della validità dati

--- js/script.js
+++ js/script.js
@@ -4,28 +4,23 @@ const junior = 18; // età massima per accedere a sconti junior
 const scontoSenior = 0.4; // sconto senior
 const scontoJunior = 0.2; //sconto junior
 
-// function debug(f) {
-//     return f;
-// }
-
-// Validazione dati utente
+// PROMPT
 alert("Grazie per aver scelto Trenord:\ninserire la propria età e i chilometri totali da percorrere (in formato numerico) per calcolare il prezzo del biglietto")
 var etaUtente = prompt('Quanti anni hai?');
-console.log(etaUtente);
 var kmPercorsi = prompt('Quanti chilometri devi percorrere?');
 
-// Debuggato tutti i potenziali problemi che vedo ora come ora
-var eBug = (etaUtente == null || etaUtente.includes("e")) || (kmPercorsi == null || kmPercorsi.includes("e"));
-var nanBug = isNaN(etaUtente * 1) || isNaN(kmPercorsi * 1);
-var valueBug = 0 >= etaUtente || etaUtente >= Infinity || 0 >= kmPercorsi || kmPercorsi >= Infinity;
+// VERIFICA VALIDITÀ DATI
+var nanBug = (isNaN(parseInt(etaUtente * 1)) || etaUtente.includes("e")) ||
+    (isNaN(parseInt(kmPercorsi * 1)) || kmPercorsi.includes("e"));
 
-if (eBug || nanBug || valueBug) {
+var valueBug = (etaUtente <= 0) || (kmPercorsi <= 0);
+var err = nanBug + valueBug;
+if (err) {
     alert('inseriti dati non validi, riprovare')
     location.reload();
 }
 
-
-// Calcolo sconto
+// CALCOLO PREZZO
 var fattorePrezzo = 1;
 var sconto = 0;
 if (etaUtente < junior) {
@@ -36,16 +31,15 @@ if (etaUtente < junior) {
     sconto = scontoSenior;
 }
 var prezzoBase = kmPercorsi * coeffKm;
-
 var prezzoFinale = prezzoBase * fattorePrezzo;
 
-// .html output
+// OUTPUT ===> .html
+document.getElementById('km').innerHTML = kmPercorsi.toLocaleString("it-IT");
 document.getElementById('prezzo-base').innerHTML = prezzoBase.toLocaleString("it-IT", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
 });
-document.getElementById('km').innerHTML = kmPercorsi.toLocaleString("it-IT");
-document.getElementById('sconto').innerHTML = (sconto * 100).toString() + "%"; // alla fine non l'ho giudicato così criptico scritto così: si capisce, no?
+document.getElementById('sconto').innerHTML = (sconto * 100).toString() + "%";
 document.getElementById('totale').innerHTML = prezzoFinale.toLocaleString("it-IT", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
